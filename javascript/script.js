a18e590a2f2f74c983f7dd8f677aa1a5ba7b0b92
mujugen <101939481+mujugen@users.noreply.github.com>
2023-03-05 17:26:27 +0800

    Cells responsive,images scaled,rose page finished

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,26 @@
+/* Function to create reproducable cells */
 function addCard(i, folder_location, container_id) {
+  /* Initialize where to put elements */
   const container = document.getElementById(container_id);
+  /* Create elements */
   const cell = document.createElement("div");
-  cell.className = "p-2 cell";
+  const price = document.createElement("p");
   const img = document.createElement("img");
+  const btn_container = document.createElement("div");
+  const btn = document.createElement("button");
   var img_filename = "images/" + folder_location + "/" + i + ".jpg";
+  /* Set class names */
+  cell.className = "p-2 cell";
   img.src = img_filename;
   img.className = "cell-img";
-  const price = document.createElement("p");
   price.className = "cell-text-title";
-  price.textContent = "P60";
-  const btn_container = document.createElement("div");
+  /* Generate random price */
+  price.textContent =
+    "P" + Math.round((Math.floor(Math.random() * 400) + 200) / 10) * 10;
   btn_container.className = "d-flex justify-content-center";
-  const btn = document.createElement("button");
   btn.className = "cell-btn";
-  btn.textContent = "Buy Now";
+  btn.textContent = "Buy";
+  /* Append all created elements */
   container.append(cell);
   cell.append(img, price, btn_container);
   btn_container.append(btn);
